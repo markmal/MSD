95c00680c5d0fc409bff81ba65ab8f85fea67b80
markmal <markmal@github.com>
2018-03-03 11:29:59 -0500

    GNU copyrights

--- SCSIDeviceClass.cpp
+++ SCSIDeviceClass.cpp
@@ -1,9 +1,26 @@
 /*
- * SCSIDevice.cpp
+ * SCSIDeviceClass.cpp
  *
  *  Created on: Feb 1, 2018
- *      Author: mmalakanov
+ *      Author: mark malakanov
  */
+/*
+  Copyright (c) 2018 Mark Malakanov.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #include "MSCDeviceClass.h"
 #include <stdint.h>
@@ -700,8 +717,6 @@ int SCSIDeviceClass::processRequest(SCSI_CBD &cbd, uint32_t len){
 		int r = processStartStop(cbd.start_stop, len);
 		return r;
 	};
-
-
 
 	//lcdConsole.println("UNSUPPORTED:"+String(cbd.generic.opcode));
 
